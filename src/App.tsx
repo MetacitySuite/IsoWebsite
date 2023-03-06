a894d8b10ddc430823587271a959bef841c6e403
Vojta Tomas <tomas@vojtatom.cz>
2023-03-06 18:58:00 +0100

    Updated font colors and margins

--- src/App.tsx
+++ src/App.tsx
@@ -14,38 +14,37 @@ function App() {
                     <div className="flex flex-row flex-wrap mx-4 rounded-3xl shadow-lg">
                         <img
                             src="dark.png"
-                            className="block   w-[50%]  object-cover rounded-tl-3xl"
+                            className="block  w-[100%] md:w-[50%]  object-cover rounded-tl-3xl rounded-tr-3xl md:rounded-tr-none"
                         />
                         <img
                             src="bridges.png"
-                            className="block  w-[50%]  object-cover rounded-tr-3xl"
+                            className="block  w-[100%] md:w-[50%]  object-cover md:rounded-tr-3xl"
                         />
                         <img
                             src="ira2.png"
-                            className="block w-[50%]  object-cover rounded-bl-3xl"
+                            className="block w-[100%] md:w-[50%]  object-cover md:rounded-bl-3xl"
                         />
                         <img
                             src="light.png"
-                            className="block w-[50%]  object-cover rounded-br-3xl"
+                            className="block w-[100%] md:w-[50%]  object-cover rounded-br-3xl rounded-bl-3xl md:rounded-bl-none"
                         />
                     </div>
                 </a>
-                <div className="max-w-[1000px] mx-auto mt-10md: mt-20 text-2xl md:text-3xl">
+                <div className="max-w-[1000px] mx-auto mt-10 md:mt-20 text-2xl md:text-3xl">
                     <p className="mx-4 leading-normal text-neutral-400">
                         We are a Prague-based division of{' '}
                         <a href="https://stdio.cz" className="border-b">
                             st.dio
                         </a>{' '}
-                        focused on spatial data visualization and simulation on the web. Check out
-                        our{' '}
-                        <a href="https://demo.metacity.cc" className="border-b text-neutral-700">
+                        programmers focused on{' '}
+                        <span className="text-neutral-700">spatial data visualization</span> and{' '}
+                        <span className="text-neutral-700">interactive simulation</span> on the web.
+                        Check out our{' '}
+                        <a href="https://demo.metacity.cc" className="border-b">
                             demo visualization projects
                         </a>{' '}
                         or our open code at{' '}
-                        <a
-                            href="https://github.com/metacitytools"
-                            className="border-b text-neutral-700"
-                        >
+                        <a href="https://github.com/metacitytools" className="border-b">
                             GitHub
                         </a>
                         .
@@ -54,21 +53,21 @@ function App() {
             </div>
 
             <div className="max-w-[1000px] mx-auto mt-10 md:mt-20">
-                <div className="flex mx-4  md:flex-row flex-col text-xl place-content-between pt-12">
+                <div className="flex mx-4  md:flex-row flex-col text-xl place-content-between">
                     <div className="text-2xl md:text-3xl">
                         <div className="mb-4">Still not sure?</div>
                         <a href="mailto:metacity@stdio.cz" className="font-black border-b-2">
                             Get in touch
                         </a>
                     </div>
-                    <div className="leading-normal mt-8 md:mt-0">
+                    <div className="leading-normal mt-8 md:mt-0 text-neutral-400">
                         <p>Prague</p>
                         <p>metacity@stdio.cz</p>
                         <p>Římská 12</p>
                         <p>120 00 Praha 2</p>
                         <p>Czech Republic</p>
                     </div>
-                    <div className="leading-normal mt-8 md:mt-0">
+                    <div className="leading-normal mt-8 md:mt-0 text-neutral-400">
                         <p>st.dio</p>
                         <p>Přimdská 362</p>
                         <p>348 02 Bor</p>
@@ -79,11 +78,11 @@ function App() {
                 </div>
             </div>
 
-            <div className="border-t py-8 mt-20 bg-slate-100">
+            <div className="border-t py-8 mt-20 bg-neutral-100">
                 <div className="max-w-[1000px] m-auto">
                     <div className="mx-4 text-sm">
                         <div className="">
-                            <div className="text-slate-400 pb-4">Partners</div>
+                            <div className="text-neutral-400 pb-4">Partners</div>
                             <div className="flex flex-row brightness-0 opacity-50">
                                 <a href="https://www.cesnet.cz">
                                     <img src="cesnet.svg" alt="cesnet" className="max-w-[100px]" />
@@ -97,7 +96,7 @@ function App() {
                                 </a>
                             </div>
                         </div>
-                        <div className="text-slate-700 my-4 mx-4">
+                        <div className="text-neutral-700 my-4">
                             &copy; 2022 st.dio s.r.o. All rights reserved, www.stdio.cz
                         </div>
                     </div>
--- src/Heading.tsx
+++ src/Heading.tsx
@@ -19,14 +19,14 @@ export function Heading() {
     }, []);
 
     return (
-        <div className="min-h-[calc(min(1000px,100vh))] max-w-[1000px] m-auto mb-5">
+        <div className="max-w-[1000px] m-auto mb-20">
             <div className="h-[calc(min(600px,60vh))] w-full relative">
                 <canvas ref={canvasRef} className="cursor-pointer z-0 rounded-3xl mt-4" />
-                <div className="absolute right-4 bottom-4 text-xs">
+                <div className="absolute right-4 bottom-4 text-xs text-neutral-400">
                     {'\u2196'} click to generate a new city
                 </div>
             </div>
-            <div className="bg-white text-2xl md:text-4xl lg:text-5xl mx-4 z-100 relative">
+            <div className="bg-white text-3xl md:text-4xl lg:text-5xl mx-4 z-100 relative">
                 <p className="leading-normal">
                     <span className="font-black">Metacity</span> offers tools and services for urban
                     data processing and visualization
