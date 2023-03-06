922caad3f262293f531cf13513ce4639b0c64872
Vojta Tomas <tomas@vojtatom.cz>
2023-03-06 17:46:22 +0100

    Rewrite?

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -19,26 +19,24 @@ export function Heading() {
     }, []);
 
     return (
-        <>
-            <div className="min-h-[calc(min(1000px,100vh))] max-w-[1000px] m-auto">
-                <div className="h-[calc(min(600px,60vh))] w-full relative">
-                    <canvas ref={canvasRef} className="cursor-pointer z-0" />
-                    <div className="absolute right-0 bottom-4">
-                        {'\u2196'} click to generate new city
-                    </div>
-                </div>
-                <div className="bg-white text-5xl leading-snug z-100 relative">
-                    <div>
-                        <span className="font-black">Metacity</span> offers tools and services for
-                        urban data processing and visualization
-                    </div>
-                    <div className="mt-8">
-                        <a href="mailto:hello@metacity.cc" className="font-black border-b-2">
-                            Get in touch
-                        </a>
-                    </div>
+        <div className="min-h-[calc(min(1000px,100vh))] max-w-[1000px] m-auto mb-5">
+            <div className="h-[calc(min(600px,60vh))] w-full relative">
+                <canvas ref={canvasRef} className="cursor-pointer z-0 rounded-3xl mt-4" />
+                <div className="absolute right-4 bottom-4 text-xs">
+                    {'\u2196'} click to generate a new city
                 </div>
             </div>
-        </>
+            <div className="bg-white text-2xl md:text-4xl lg:text-5xl mx-4 z-100 relative">
+                <p className="leading-normal">
+                    <span className="font-black">Metacity</span> offers tools and services for urban
+                    data processing and visualization
+                </p>
+                <p className="mt-8">
+                    <a href="mailto:metacity@stdio.cz" className="font-black border-b-2">
+                        Get in touch
+                    </a>
+                </p>
+            </div>
+        </div>
     );
 }
